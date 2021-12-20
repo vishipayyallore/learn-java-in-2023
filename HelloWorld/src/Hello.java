8d8d33dc5c0be1939df43598f18494c284d6e1ef
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2021-12-20 18:42:29 +0000

    Update Hello.java

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,9 +1,16 @@
+import java.text.MessageFormat;
 
 public class Hello {
 
     public static void main(String[] args){
 
+        int value1 = 10;
+        int value2 = 20;
+        int sum = value1 + value2;
+
         System.out.println("Hello World !!");
         System.out.println("Hello World Second Time !!");
+
+        System.out.println(MessageFormat.format("{0} + {1} = {2}", value1, value2, sum));
     }
 }
