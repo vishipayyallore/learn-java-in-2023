387da7dcca13e3b89cce9b99b4c58c6bec24a75c
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2022-08-02 20:06:21 +0530

    Create PropertyDto.java

---
+++ property-management/src/main/java/com/echannel/propertymanagement/dtos/PropertyDto.java
@@ -0,0 +1,59 @@
+package com.echannel.propertymanagement.dtos;
+
+public class PropertyDto {
+
+    private Long id;
+    private String title;
+    private String description;
+    private Double price;
+    private String address;
+    private Long userId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+}
