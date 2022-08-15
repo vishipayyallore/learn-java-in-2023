9dd1fefb050535caab01d2a4fcdc20341f4fc322
Viswanatha Swamy <vishipayyallore@yahoo.com>
2022-08-16 00:10:06 +0530

    Swamy/15aug2022 save new property endpoint (#1)
    * Update PropertiesController.java
    
    * Added Simple Service, and Controller Method

--- property-management/src/main/java/com/echannel/propertymanagement/controllers/PropertiesController.java
+++ property-management/src/main/java/com/echannel/propertymanagement/controllers/PropertiesController.java
@@ -1,13 +1,26 @@
 package com.echannel.propertymanagement.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.echannel.propertymanagement.dtos.PropertyDto;
+import com.echannel.propertymanagement.services.PropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/properties")
 public class PropertiesController {
 
+    @Autowired
+    private PropertyService propertyService;
 
+    @PostMapping("")
+    public ResponseEntity<PropertyDto> saveProperty(@RequestBody PropertyDto propertyDto){
+
+        propertyDto = propertyService.saveProperty(propertyDto);
+
+        ResponseEntity<PropertyDto> responseEntity = new ResponseEntity<>(propertyDto, HttpStatus.CREATED);
+        return responseEntity;
+    }
 
 }
---
+++ property-management/src/main/java/com/echannel/propertymanagement/services/PropertyService.java
@@ -0,0 +1,9 @@
+package com.echannel.propertymanagement.services;
+
+import com.echannel.propertymanagement.dtos.PropertyDto;
+
+public interface PropertyService {
+
+    PropertyDto saveProperty(PropertyDto propertyDto);
+
+}
