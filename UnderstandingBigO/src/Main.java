ac1c5034cda1279bfd0ecdb8acdeaa8f4e9d49fb
Viswanatha Swamy P K <vishipayyallore@yahoo.com>
2023-03-22 21:13:39 +0530

    Update Main.java

--- UnderstandingBigO/src/Main.java
+++ UnderstandingBigO/src/Main.java
@@ -2,13 +2,32 @@ public class Main {
     public static void main(String[] args) {
 
         printItems(10);
+
+        printItemsV1(10);
+
         System.out.println("Hello world!");
     }
 
-    // O(n)
-    public static void printItems(int n){
-        for(int i=0; i <n; i++){
+    // O(n). - Drop Constants - O(n) == O(n)
+    public static void printItems(int n) {
+        for (int i = 0; i < n; i++) {
             System.out.println(i);
         }
+
+        for (int j = 0; j < n; j++) {
+            System.out.println(j);
+        }
     }
+
+    // O(n). - O(n^2) 
+    public static void printItemsV1(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) { // n * n === O(n^2)
+                for (int k = 0; k < n; k++) { // n * n * n === O(n^3) === O(n^2)
+                    System.out.println(i + " " + j + " " + k);
+                }
+            }
+        }
+    }
+
 }
