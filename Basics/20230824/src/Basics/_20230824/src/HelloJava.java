f7c4794447fa731a1a3d3973388a21cb30201e57
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-24 22:48:42 +0530

    Changes

---
+++ Basics/20230824/src/Basics/_20230824/src/HelloJava.java
@@ -0,0 +1,10 @@
+// package Basics.20230824;
+
+package Basics._20230824.src;
+
+public class HelloJava {
+    
+    public static void main(String[] args) {
+        System.out.println("Hello Java!");
+    }
+}
