14939179d584663201f8730f91721607828b5adf
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-26 21:37:20 +0530

    Changes

---
+++ Basics/20230824/src/IntTypeDemo/IntDemos.java
@@ -0,0 +1,16 @@
+package IntTypeDemo;
+
+public class IntDemos {
+
+    public static void main(String[] args) {
+        System.out.println("Integer Data Type Demo.");
+
+        int age = 18;
+        String name = "Sam";
+        System.out.println(String.format("%s is %d years old.", name, age));
+
+        age = 18 + 1;
+        name = "John";
+        System.out.println(String.format("%s is %d years old.", name, age));
+    }
+}
