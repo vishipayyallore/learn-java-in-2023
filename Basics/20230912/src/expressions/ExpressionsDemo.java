070c4ec8ede06030901bdf396a9efd07fe8bb078
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-12 20:38:04 +0530

    Create ExpressionsDemo.java

---
+++ Basics/20230912/src/expressions/ExpressionsDemo.java
@@ -0,0 +1,13 @@
+package expressions;
+
+public class ExpressionsDemo {
+    
+        public static void main(String[] args) {
+    
+            int a = 10;
+            int b = 20;
+    
+            int sum = new ExpressionsDemo().addTwoNumbers(a, b);
+            System.out.println("Sum of " + a + " and " + b + " is " + sum);
+        }
+}
