bb95489b4cb0b205aa3292742d62b56b77aca67b
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-13 10:09:27 +0530

    Swamy/12sep primitive types (#11)
    * Create ExpressionsDemo.java
    
    * Update ExpressionsDemo.java
    
    * Update ExpressionsDemo.java

---
+++ Basics/20230912/src/expressions/ExpressionsDemo.java
@@ -0,0 +1,12 @@
+package expressions;
+
+public class ExpressionsDemo {
+    
+        public static void main(String[] args) {
+    
+            int a = 10;
+            int b = 20;
+    
+            System.out.println("Sum of " + a + " and " + b + " is " );
+        }
+}
